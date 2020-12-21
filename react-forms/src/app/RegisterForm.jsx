fbb3039a1b09af84da7a9157982f5e929823ea9e
Max Eineder <max.eineder@googlemail.com>
2020-12-21 15:56:01 +0100

    Add more widgets

--- react-forms/src/app/RegisterForm.jsx
+++ react-forms/src/app/RegisterForm.jsx
@@ -5,18 +5,46 @@ export class RegisterForm extends React.PureComponent {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.handleTShirtSizeChange = this.handleTShirtSizeChange.bind(this);
+        this.handleLunchChange = this.handleLunchChange.bind(this);
         this.state = {
-            name: ''
+            name: '',
+            tShirtSize: 'm-l',
+            lunch: true
         };
 
     }
 
     render() {
         return (
-            <input type="text"
-                value={this.state.name}
-                onChange={this.handleChange}
-            />
+            <div>
+                <label>Name:
+                <input type="text"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <br />
+                <label>Size:
+                    <select value={this.state.tShirtSize}
+                        onChange={this.handleTShirtSizeChange}>
+                        <option value="w-s">W-S</option>
+                        <option value="w-m">W-M</option>
+                        <option value="w-l">W-L</option>
+                        <option value="m-s">M-S</option>
+                        <option value="m-m">M-M</option>
+                        <option value="m-l">M-L</option>
+                    </select>
+                </label>
+                <br />
+
+                <label>Lunch:
+                    <input type="checkbox"
+                        checked={this.state.lunch}
+                        onChange={this.handleLunchChange}
+                    />
+                </label>
+            </div>
         );
     }
 
@@ -28,4 +56,15 @@ export class RegisterForm extends React.PureComponent {
         });
     }
 
+    handleTShirtSizeChange(event) {
+        this.setState({
+            tShirtSize: event.target.value
+        });
+    }
+
+    handleLunchChange(event) {
+        this.setState({
+            lunch: event.target.checked
+        });
+    }
 }
